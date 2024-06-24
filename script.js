3203139173bba6c4f7b0350dd17b125a2df8d9b8
maureen <maureenotieno38@gmail.com>
2024-06-24 17:40:20 +0300

    complete work

--- script.js
+++ script.js
@@ -2,10 +2,25 @@
 const modal=document.querySelector('.modal');
 const overlay=document.querySelector('.overlay');
 const btnCloseModal=document.querySelector('.close-modal');
-const btnsOpenModal=document.querySelectorAll('.show-modal');
-for(let i=0;i<btnsOpenModal.length;i++)
-btnsOpenModal[i].addEventListener('click',function(){
+const btnsOpenModal=document.querySelectorAll('.display-modal');
+// console.log(btnsOpenModal);
+const closeModal=function(){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+const openModal=function(){
     modal.classList.remove('hidden');
     overlay.classList.remove('hidden');
-console.log('button clicked');
+}
+for(let i=0;i<btnsOpenModal.length;i++)
+   btnsOpenModal[i].addEventListener('click',openModal);
+
+
+btnCloseModal.addEventListener('click',closeModal);
+overlay.addEventListener('click',closeModal)
+document.addEventListener('keydown',function(e){
+    // console.log(e.key)
+    if(e.key=='Escape' && !modal.classList.contains('hidden') ){
+     closeModal();            }
+    
 });
